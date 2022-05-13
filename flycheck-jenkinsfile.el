503e7de90a2cb62b0da3f9aa9e33d4b08cbdbefd
CsBigDataHub <kchetan.hadoop@gmail.com>
2022-05-13 13:55:45 -0500

    Disabled CRUMB for Jenkins [Personal preference]

--- flycheck-jenkinsfile.el
+++ flycheck-jenkinsfile.el
@@ -21,23 +21,23 @@
   :safe #'stringp
   :group 'flycheck-jenkinsfile)
 
-(defcustom flycheck-jenkinsfile-jenkins-crumb ""
-  "The crumb."
-  :type 'string
-  :safe #'stringp
-  :group 'flycheck-jenkinsfile)
+;; (defcustom flycheck-jenkinsfile-jenkins-crumb ""
+;;   "The crumb."
+;;   :type 'string
+;;   :safe #'stringp
+;;   :group 'flycheck-jenkinsfile)
 
 (flycheck-define-checker
     jenkinsfile
   "A Jenkins declarative pipeline checker."
   :command ("curl"
-            (option "-H" flycheck-jenkinsfile-jenkins-crumb)
+;;             (option "-H" flycheck-jenkinsfile-jenkins-crumb)
             (eval (when buffer-file-name
                     (concat "-F jenkinsfile=<" buffer-file-name)))
             (eval (concat flycheck-jenkinsfile-jenkins-url "/pipeline-model-converter/validate")))
   :error-patterns (
                    (error line-start "WorkflowScript: " line ": " (message) "@ line " line ", column " column ".")
                    )
-  :modes groovy-mode)
+  :modes (groovy-mode jenkinsfile-mode))
 
 (add-to-list 'flycheck-checkers 'jenkinsfile)
